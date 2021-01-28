8b20c5c3f3ae99001628bc6f2cefe3dd6a24d3bb
Rhiddhi Prasad Das <rhiddhiprasad@gmail.com>
2021-01-28 20:38:58 +0530

    Update pdoData.php

--- pdoData.php
+++ pdoData.php
@@ -3,5 +3,5 @@ $dbhs = "";//host
 $dbus = "";//username
 $dbpw = "";//password
 $dbnm = "heybro";
-$pdo=new PDO('mysql:host='.$dbhs.';port=3306;dbname='.$dbnm.';charset=utf8',$dbus,$dbpw);
+$pdo=new PDO('mysql:host='.$dbhs.';port=3306;dbname='.$dbnm.';charset=utf8mb4',$dbus,$dbpw);
 ?>
