baf069a23156232b2844fb980b2b8b1e77c5338a
Rhiddhi Prasad Das <rhiddhiprasad@gmail.com>
2022-07-22 16:42:18 +0530

    Update mysqlFile.sql

--- mysqlFile.sql
+++ mysqlFile.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `friends`
 --
 
-DROP TABLE IF EXISTS `friends`;
+/*DROP TABLE IF EXISTS `friends`;*/
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `friends` (
@@ -27,7 +27,7 @@ CREATE TABLE `friends` (
   `frndId` int DEFAULT NULL,
   `userId` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +36,6 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-INSERT INTO `friends` VALUES (28,26,27),(30,26,28),(31,27,28),(33,28,26),(36,26,30),(37,30,26),(38,27,26);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -53,7 +52,7 @@ CREATE TABLE `messages` (
   `recvId` int DEFAULT NULL,
   `mesg` text,
   PRIMARY KEY (`msgId`)
-) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +61,6 @@ CREATE TABLE `messages` (
 
 LOCK TABLES `messages` WRITE;
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` VALUES (122,30,26,'Hi'),(123,26,30,'Hello'),(124,30,26,'Mukh dhue ae'),(125,26,30,'Hello'),(126,26,30,'Hi'),(127,26,30,'Lemon achho'),(128,26,30,'Hola'),(129,26,30,'Hello bapi'),(130,30,26,'Yes'),(131,26,30,'Kemon achho'),(132,30,26,'Valo'),(133,30,26,'Tui'),(134,26,30,'Bhalo');
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,7 +78,7 @@ CREATE TABLE `userData` (
   `usr` text,
   `pswd` text,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,7 +87,6 @@ CREATE TABLE `userData` (
 
 LOCK TABLES `userData` WRITE;
 /*!40000 ALTER TABLE `userData` DISABLE KEYS */;
-INSERT INTO `userData` VALUES (26,'rhiddhiprasad@gmail.com','rhiddhi prasad das','511178','a87bb8fda48e3a08dd0af858a9184a20'),(27,'agentpluton@gmail.com','agentp','025637','a87bb8fda48e3a08dd0af858a9184a20'),(28,'qwerty@gmail.com','qwertyMan','351173','a87bb8fda48e3a08dd0af858a9184a20'),(29,'Dfg@red.com','ব','897559','70a902979b091f6a170510e1b4458ee9'),(30,'Bani@gmaul.com','Bani','699757','4508c2be67203ab3850f81043e12c18e');
 /*!40000 ALTER TABLE `userData` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
